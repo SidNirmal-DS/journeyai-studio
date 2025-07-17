0cf80b2e0159b02fe02c13f5901afe0e6cae6355
SidNirmal-DS <68082173+SidNirmal-DS@users.noreply.github.com>
2025-07-17 22:12:33 +0530

    Prepare static export for GitHub Pages

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true
+  reactStrictMode: true,
+  output: 'export',
+  basePath: '/journeyai-studio',
+  images: {
+    unoptimized: true,
+  },
 };
 
 module.exports = nextConfig; 
